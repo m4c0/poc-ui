f9752ab8ac7f0ee7145b28daf1ece7f6904f993c
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-29 14:06:50 +0100

    build on windows

---
+++ build-win.c
@@ -0,0 +1,84 @@
+#include <sys/stat.h>
+#include <assert.h>
+#include <direct.h>
+#include <process.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define OPT "-gdwarf"
+//#define OPT "-O3"
+
+static void usage() {
+  fprintf(stderr, "just call 'build' without arguments\n");
+}
+
+static int run(char ** args) {
+  assert(args && args[0]);
+
+  if (0 == _spawnvp(_P_WAIT, args[0], (const char * const *)args)) {
+    return 0;
+  }
+
+  fprintf(stderr, "failed to run child process: %s\n", args[0]);
+  return 1;
+}
+
+static int shader(char * name) {
+  char spv[1024]; snprintf(spv, 1024, "app/%s.spv", name);
+  char src[1024]; snprintf(src, 1024, "../%s", name);
+
+  char * args[] = { "glslang", "-V", src, "-o", spv, 0 };
+  return run(args);
+}
+
+static int pch() {
+  char * args[] = {
+    "clang", "-Wall", OPT, "-x", "c-header",
+    "-IVulkan-Headers/include",
+    "-D", "VK_USE_PLATFORM_WIN32_KHR",
+    "-D", "VLK_USE_VOLK",
+    "-o", "pch.pch", "pch.h", 0 };
+  return run(args);
+}
+
+static int cc(char * src, char * o) {
+  char * args[] = {
+    "clang", "-Wall", OPT, "-include-pch", "pch.pch",
+    "-o", o, "-c", src, 0 };
+  return run(args);
+}
+
+static int hdr(char * src, char * o, char * d) {
+  char * args[] = {
+    "clang", "-Wall", "-x", "c", OPT, "-include-pch", "pch.pch",
+    "-D", d, "-o", o, "-c", src, 0
+  };
+  return run(args);
+}
+
+static int link_hello() {
+  char * args[] = {
+    "clang", "-Wall", OPT,
+    "microui.o", "hello.o", "hello-win.o",
+    "-o", "app/sokoban.exe", "-lole32", "-luser32",
+    0 };
+  return run(args);
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 1) return (usage(), 1);
+
+  _mkdir("app");
+
+  if (pch()) return 1;
+
+  if (hdr("volk.h", "volk.o", "VOLK_IMPLEMENTATION")) return 1;
+
+  if (cc("microui.c", "microui.o")) return 1;
+
+  if (cc("hello.c", "hello.o")) return 1;
+  if (cc("hello-win.m", "hello-win.o")) return 1;
+  if (link_hello()) return 1;
+
+  return 0;
+}
